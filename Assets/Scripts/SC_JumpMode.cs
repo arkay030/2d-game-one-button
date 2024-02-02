3499d00cb443cb7e04fb37a603798c99f9c230c9
ArnaudMorks <arnaud.morks@gmail.com>
2024-02-02 10:13:11 +0100

    A veel stuf

--- Assets/Scripts/SC_JumpMode.cs
+++ Assets/Scripts/SC_JumpMode.cs
@@ -9,7 +9,6 @@ public class SC_JumpMode : SC_CharacterBase
     protected override void Start()
     {
         base.Start();
-        runSpeedBase = 2.6f;
         movementX = 1;
     }
 
@@ -17,10 +16,22 @@ public class SC_JumpMode : SC_CharacterBase
     {
         base.Update();
 
-        if (Input.anyKeyDown && isGrounded)
+        if (Input.anyKeyDown && isGrounded && canJump)
         {
             canJump = false;
             Jump();
+        }
+
+        if (canJump == false && jumpCooldown >= 0)              //jump Cooldown timer
+        {
+            jumpCooldown -= Time.deltaTime;
+            // Debug.Log(canJump);
+        }
+        else if (canJump == false)
+        {
+            canJump = true;
+            jumpCooldown = jumpCooldownBase;
+            print("Can attack again");
         }
 
     }
